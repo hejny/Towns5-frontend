f7de38f7bb681e52752262d9de960fa8ba0e3da9
phejny <ph@towns,cz>
2016-02-29 01:36:50 +0100

    [webgl][PH] Offline mode

---
+++ app/js/classes/townsapi-offline.class.js
@@ -0,0 +1,75 @@
+/**
+ * @author ©Towns.cz
+ * @fileOverview Towns API Wrapper
+ */
+//======================================================================================================================token
+
+TownsAPIOffline=function(){
+
+};
+
+//======================================================================================================================
+
+/**
+ * @private
+ * @param uri
+ * @param method
+ * @param data
+ * @param callback_success
+ * @param callback_error
+ * @returns {object} jQuery $.ajax
+ */
+TownsAPIOffline.prototype.query = function(uri,query_data,method,data,callback_success=false,callback_error=false){
+
+
+    if(callback_success)
+        callback_success([]);
+
+
+    return(false);
+
+
+};
+
+
+//=================================================
+
+/**
+ *
+ * @param uri
+ * @param data
+ * @param callback_success
+ * @param callback_error
+ * @returns {object} jQuery $.ajax
+ */
+TownsAPIOffline.prototype.get = function(uri,query_data,callback_success,callback_error){return this.query(uri,query_data,'GET',{},callback_success,callback_error);};
+
+/**
+ *
+ * @param uri
+ * @param data
+ * @param callback_success
+ * @param callback_error
+ * @returns {object} jQuery $.ajax
+ */
+TownsAPIOffline.prototype.post = function(uri,object,callback_success,callback_error){
+
+    return this.query(uri,{},'POST',object,callback_success_wrapped,callback_error);
+
+
+};
+
+/**
+ *
+ * @param uri
+ * @param callback_success
+ * @param callback_error
+ * @returns {Object}
+ */
+TownsAPIOffline.prototype.delete = function(uri,callback_success,callback_error){
+
+    return this.query(uri,{},'DELETE',{},callback_success,callback_error);
+
+};
+
+
