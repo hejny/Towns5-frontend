e6948c7050bb85d078129cf73bd95041730384b6
Pavol Hejný <me@pavolhejny.com>
2016-03-16 17:04:47 +0100

    [game][PH] object profile

--- app/js/classes/resources.class.js
+++ app/js/classes/resources.class.js
@@ -20,7 +20,7 @@ Resources.prototype.toHTML = function(){
 
                 value=value.toLocaleString(/*'en-US''de-DE'*/);//todo todo better solution
 
-                strings.push('<div><img src="media/image/resources/'+key+'.png" title="'+name+'" alt="'+name+'" >'+value+'</div>');
+                strings.push('<div><img src="/media/image/resources/'+key+'.png" title="'+name+'" alt="'+name+'" >'+value+'</div>');
             }
 
         }
--- app/js/plugins/viewers/profile.viewer.js
+++ app/js/plugins/viewers/profile.viewer.js
@@ -10,12 +10,54 @@ T.Plugins.install(new T.Viewer(
     ,{}
     ,'Profil'
     ,`
-    name: <div id="profile-viewer-name">name</div>
+
+    <div class="page-column-2">
+        <div id="profile-viewer-price">name</div>
+    </div>
+
+    <div class="page-column-2">
+        <div id="model-canvas"></div>
+    </div>
+
     `
     ,function(object){
 
 
-        $('#profile-viewer-name').text(object.name);
+        new ModelCanvas('model-canvas',object.design.data,'100%','100%',null,1);
+
+
+        var price = game.getObjectPrice(object).toHTML();
+        var maxlife = game.getObjectMaxLife(object).toLocaleString();
+
+
+        var html='<table  class="full_width">';
+
+
+
+        html+='<tr><th colspan="2"><h2>'+object.name+'</h2></th></tr>';
+        html+='<tr><td>'+Locale.get('object','price')+'</td><td>'+price+'</td></tr>';
+        html+='<tr><td>'+Locale.get('object','maxlife')+'</td><td>'+maxlife+'</td></tr>';
+
+
+
+        object.actions.forEach(function(action){
+
+            html+='<tr><th colspan="2">'+Locale.get('action',action.type)+'</th></tr>';
+
+            for(var key in action.params){
+
+                html+='<tr><td>'+Locale.get('action',action.type,key)+'</td><td>'+action.params[key].toLocaleString()+'</td></tr>';
+
+            }
+
+
+        });
+
+        html+='</table>';
+
+        $('#profile-viewer-price').html(html);
+
+
 
 
     }
