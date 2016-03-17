72f2bf394e063f86327a2ff54c380e3d666ba06b
Pavol Hejný <ph@towns.cz>
2016-03-17 16:18:09 +0100

    [game][PH] reporting system

---
+++ app/js/functions/track-event.function.js
@@ -0,0 +1,27 @@
+//todo header
+
+
+
+//======================================================================================================================
+
+
+/**
+ * Towns event tracking system
+ * @author PH
+ * @param {string} category eg. function, ui,..
+ * @param {string} action eg. terrain changing, dismantling,...
+ * @param additional params eg. t10, building_market,...
+ */
+function trackEvent(category, action , additional){
+
+    r('Tracked event '+category+' / '+action+' !');
+
+
+    ga('send', 'event', category, action /*'Fall Campaign'*/);
+
+
+
+}
+
+
+
