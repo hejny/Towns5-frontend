bf0e3af7a1a802fa4f8b6f96aa38aaad9461f6d0
Pavol Hejný <ph@towns.cz>
2016-03-19 01:25:47 +0100

    [game][PH] Story editor

--- app/js/plugins/editors/story.editor.js
+++ app/js/plugins/editors/story.editor.js
@@ -20,54 +20,119 @@ T.Plugins.install(new T.Editor(
 
     `,function(object){
 
+        //---------------------------------------------------------
 
-        separator_width=5;//Math.toInt($('#vertical_separator').scss('width'));
-        separator_border=2;//Math.toInt($('#story-content').scss('border-right'));
-        window_padding=20;//Math.toInt($('.popup-window .content').scss('padding'));
+        var window_width=750;//todo refactor rename and move to vars
+        var separator_bound=10;
+        var separator_snap=100;
 
-        //r('storywrite',separator_width,separator_border,window_padding,$('#vertical_separator'),$('#story-content').scss('border-right'),$('.popup-window .content'));
+        var separator_width=5;//Math.toInt($('#vertical_separator').scss('width'));
+        var separator_border=2;//Math.toInt($('#story-content').scss('border-right'));
+        var window_padding=20;//Math.toInt($('.popup-window .content').scss('padding'));
 
-        $('#vertical_separator').css('left',window_width/2+separator_width/2-window_padding);
+        //---------------------------------------------------------
+
+        var storyContentWidthReload = function(){
+
+            var width1 = Math.toInt($('#vertical_separator').css('left'));
+
+            if(isNaN(width1))width1=0;
+
+            /*if(width1<separator_snap){
+                width1=separator_bound;
+                $('#vertical_separator').css('left',width1);
+                $('#story-content').hide();
+                $('#story-content-html').show();
+            }else
+            if(width1>window_width-separator_snap+separator_width){
+                width1=window_width-separator_bound+separator_width;
+                $('#vertical_separator').css('left',width1);
+                $('#story-content').show();
+                $('#story-content-html').hide();
+            }else
+            {
+                $('#story-content').show();
+                $('#story-content-html').show();
+            }*/
+
+            if(width1<separator_snap){
+
+                width1=separator_snap;
+                $('#vertical_separator').css('left',width1);
 
 
+            }else
+            if(width1>window_width-separator_snap+separator_width){
+
+                width1=window_width-separator_snap+separator_width;
+                $('#vertical_separator').css('left',width1);
 
 
-        $('#vertical_separator').draggable({
+            }
+
+
+            var width2 = window_width-width1;
+
+
+            $('#story-content').css('width',width1-window_padding-separator_width-2*separator_border);
+            $('#story-content-html')
+                .css('width',width2)
+                .css('left',width1+separator_width);
+
+
+            $('#story-content').show();
+            $('#story-content-html').show();
+            $('#vertical_separator').show();
+
+        };
+
+
+        var storyContentReload = function(){
+
+            object.content.data = $('#story-content').val();
+
+            var story_content_html = markdown.toHTML(object.content.data);
+            $('#story-content-html').html(story_content_html);
+
+        };
+
+
+        //---------------------------------------------------------
+
+
+        $('#vertical_separator')
+            .css('left',window_width/2+separator_width/2-window_padding)
+            .draggable({
             axis: 'x',
             drag: storyContentWidthReload,
             stop: storyContentWidthReload
 
         });
 
-        /*var story_name='Nazev';
-         var story_content=[
-         'Nadpis',
-         '=========',
-         '',
-         'text text text',
-         ''].join('\n');*/
 
+        $('#story-content').val(object.content.data).keypress(function(){
 
-        var i = ArrayFunctions.id2i(objects_external,map_selected_ids[0]);
-        r(map_selected_ids,i);
+            setTimeout(function(){
+                storyContentReload();
+            },IMMEDIATELY_MS);
 
-        $('#story-name').val(objects_external[i].name);
-        $('#story-content').val(objects_external[i].content.data);
+        });
 
 
         storyContentReload();
         storyContentWidthReload();
 
+        //---------------------------------------------------------
 
 
     },
     {
-        "name": "Příběh",
+        "name": "Story",
         "type": "story",
-        "locale": "cs",
+        "locale": "en",
         "content": {
             "type": "markdown",
-            "data": "Kde bolo tam bolo"
+            "data": ""
         }
     }
 
@@ -80,80 +145,8 @@ T.Plugins.install(new T.Editor(
 //======================================================================================================================
 
 
-var window_width=780;//todo refactor rename and move to vars
-separator_bound=10;
-separator_snap=100;
-var separator_width,separator_border,window_padding;
 
 
-/*Pages.story_editor.closeJS = function(){
-
- map_selected_ids=[];
- Map.loadMap();
-
- };*/
-
-
-var storyContentWidthReload = function(){
-
-    var width1 = Math.toInt($('#vertical_separator').css('left'));
-
-    if(isNaN(width1))width1=0;
-
-    if(width1<separator_snap){
-        width1=separator_bound;
-        $('#vertical_separator').css('left',width1);
-        $('#story-content').hide();
-        $('#story-content-html').show();
-    }else
-    if(width1>window_width-separator_snap+separator_width){
-        width1=window_width-separator_bound+separator_width;
-        $('#vertical_separator').css('left',width1);
-        $('#story-content').show();
-        $('#story-content-html').hide();
-    }else
-    {
-        $('#story-content').show();
-        $('#story-content-html').show();
-    }
-
-
-    var width2 = window_width-width1;
-
-    //r(window_padding,separator_width,separator_border);
-    //r(width1-window_padding-separator_width-2*separator_border,width2,width1+separator_width);
-
-
-    $('#story-content').css('width',width1-window_padding-separator_width-2*separator_border);
-    $('#story-content-html').css('width',width2);
-    $('#story-content-html').css('left',width1+separator_width);
-
-
-    $('#story-content').show();
-    $('#story-content-html').show();
-    $('#story-content-html').show();
-    $('#vertical_separator').show();
-
-
-    r('Uzzzzzzzzzzzz');
-
-};
-
-
-
-var storyContentReload = function(){
-
-    var story_content = $('#story-content').val();
-
-    var story_content_html = markdown.toHTML(story_content);
-
-
-    $('#story-content-html').html(story_content_html);
-
-
-
-
-};
 
 
 
