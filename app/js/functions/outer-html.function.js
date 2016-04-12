d558ad269ac1124e62fb09f25708b4fabe5c2b08
Pavol Hejný <me@pavolhejny.com>
2016-04-12 23:24:10 +0200

    [PH] jQuery outerHTML repair

--- app/js/functions/outer-html.function.js
+++ app/js/functions/outer-html.function.js
@@ -1,8 +1,18 @@
 //todo header
 
-//todo is this function needed?
+//todo how to do jsdoc in jQuery functions (plugins)?
+/**
+ * Converts JQuery object to html code
+ * @returns {string}
+ */
 jQuery.fn.outerHTML = function(s) {
-    return s
-        ? this.before(s).remove()
-        : jQuery("<p>").append(this.eq(0).clone()).html();
+
+    var html='';
+    for(var i= 0,l=this.length;i<l;i++){
+        if( typeof this[i].outerHTML=== 'string' ){
+            html+=this[i].outerHTML;
+        }
+    }
+    return(html);
+
 };
