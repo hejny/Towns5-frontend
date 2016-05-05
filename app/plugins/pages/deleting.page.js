6d8321dc165719550f26ce1773b4d14829f617df
pavolhejny <ph@towns.cz>
2016-05-06 01:51:13 +0200

    [PH] deleting page

--- app/plugins/pages/deleting.page.js
+++ app/plugins/pages/deleting.page.js
@@ -10,101 +10,81 @@
 
 
 T.Plugins.install(new T.Plugins.Page(
-    'feedback',
-    T.Locale.get('page','feedback'),
+    'deleting',
+    T.Locale.get('page','deleting'),
     `
-
-
-
- <form id="feedback-form" class="form-big">
-
-    <table>
-
-
-        <tr>
-            <td>`+T.Locale.get('user','email')+`:</td>
-            <td><input style="width: 100%;" type="email" name="email"  autofocus></td>
-        </tr>
-
-
-        <tr>
-            <td colspan="2"><textarea name="text"  style="width:100%;height: 130px;"></textarea></td>
-        </tr>
-
-        <tr>
-            <td colspan="2">
-            <button style="width: 150px;">`+T.Locale.get('send')+`</button>
-            </td>
-        </tr>
-
-    </table>
-
-
-</form>
+<ul class="news">
+</ul>
 
 
 `
-    ,function(){
+    ,function(page){
+
+
+        T.TownsAPI.townsAPI.get('stories',{latest:true},function(result){
+
+            var html='',item_html;
+
+            var stories = new T.Objects.Array(result);
+
+
+            stories.forEach(function(story){
+
+                objects_external.update(story);
 
 
 
-        $('#feedback-form').find('button').click(function(){
-            $('#feedback-form').trigger('submit');
+                var content=story.content.data;
+                content = markdown.toHTML(content);
+
+
+                var image = $(content).find('img:first').attr('src');
+                if(image) {
+
+
+                    image = URI(image)
+                        .removeSearch("width")
+                        .addSearch({width: 100})
+                        .toString()
+                    ;
+
+
+                }
+
+                var item = {
+                    type: 'story',
+                    onclick: "deleteObject('"+story.id+"');$(this).css('opacity',0.2);",
+                    image: image,
+                    title: story.name,
+                    date: new Date(story.start_time),
+                    target: '_self'
+                };
+
+                if(!item.image){
+                    item.image='/media/image/icons/view.svg';
+                }
+
+                item_html=`
+                <li onclick="`+item.onclick+`">
+                        <img src="`+item.image+`">
+                        <h2 class="title">`+item.title+`</h2>
+                        <p class="type">`+ T.Locale.get('news','type',item.type)+(item.target=='_blank'?'<i class="fa fa-external-link"></i>':'')+`</p>
+                        <p class="date">`+dateToSmartString(item.date)+`</p>
+                        <p>`+content+`</p>
+                </li>
+                `;
+
+                html+=item_html;
+
+            });
+
+            $(page).find(".news").html(html);
         });
 
 
-        $('#feedback-form').submit(function(e){
-
-            e.preventDefault();
-
-            var data={};
-
-            //todo form to json
-            $(this).find('input,textarea').each(function(){
-
-                if($(this).attr('type')=='submit')return;
-
-                var key=$(this).attr('name');
-                key=key.split('-').join('_');
-
-                //r(key);
-
-                data[key]=$(this).val();
-
-            });
-
-
-            $('#feedback-form').find('button').html(T.Locale.get('loading')+' <i class="fa fa-spinner faa-spin animated"></i>');
-
-
-            r(data);
-
-            var request = $.post(
-                appDir+'/php/feedback.php',
-                data
-            );
-            request.done(function( response ){
-
-                $('#login-form').find('button').html(T.Locale.get('send'));
-                T.UI.popupWindow.close();
-                T.UI.Messages.message(T.Locale.get('feedback','success'),'success');
-
-            });
-            request.fail(function( response ){
-
-            });
 
 
 
-
-        });
-
-
-
-
-
-    },
-    undefined,
-    'SMALL'
+    }
 ));
 
